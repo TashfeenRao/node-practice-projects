be17a0534206692bfe3ece6ee80e07e708c63ad5
Tashfeen Rao <tashfeen@ricult.com>
2023-04-30 22:06:23 +0500

    added seo friendly url, pagination

--- blog-site/playground/views.py
+++ blog-site/playground/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator
 from .models import Post
 # Create your views here.
 
@@ -7,12 +8,14 @@ def hello_world(request):
     return HttpResponse("hello world")
 
 def posts_list(request):
+    posts_list = Post.objects.all().filter()
 
-    posts = Post.objects.all()
-
+    paginator = Paginator(posts_list,3)
+    page_num = request.GET.get('page',1)
+    posts = paginator.page(page_num)
     return render(request,'blog/post/list.html',{'posts':posts})
 
-def post_detail(request,id):
-    post = Post.objects.get(id=id)
+def post_detail(request,year,month,day,post):
+    post = get_object_or_404(Post, publish__year=year, publish__month=month, publish__day=day, slug=post)
 
     return render(request,'blog/post/detail.html',{'post': post})
